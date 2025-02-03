45852a67927221f74e632876670abeeb4d3c0a8a
Daiki <daiki01miura04@gmail.com>
2025-02-03 23:32:39 +0900

    fix: error

---
+++ src/electron.d.ts
@@ -0,0 +1,10 @@
+interface Window {
+  electron: {
+    saveTimeEntry: (data: {
+      projectId: string;
+      startTime: string;
+      endTime: string;
+      duration: number;
+    }) => Promise<void>;
+  }
+}
